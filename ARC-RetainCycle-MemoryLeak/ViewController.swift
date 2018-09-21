8157bd87bacc0d897dfc087d5aea36ec388d7226
Workerlv <arturs280@inbox.lv>
2018-09-21 20:17:52 +0300

    Initial Commit

---
+++ ARC-RetainCycle-MemoryLeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARC-RetainCycle-MemoryLeak
+//
+//  Created by Arturs Vitins on 21/09/2018.
+//  Copyright © 2018 Arturs Vitins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
